e93fecd6c59daf629f7e2ae7be7589acd1299c6d
Dolan <dolan_miu@hotmail.com>
2017-08-08 18:19:13 +0100

    Comment out parts

--- src/cli.ts
+++ src/cli.ts
@@ -10,22 +10,23 @@ const WORKING_DIR = `./node_modules/${PACKAGE_NAME}/dist/working-dir`;
 const setup = new SetUp(WORKING_DIR);
 const angularCli = new AngularCLI(WORKING_DIR);
 
+console.log("Clearing working directory...");
 setup.clearWorkingDirectory();
-
+console.log("Copying files...");
 setup.setup();
-
+// console.log("Installing...");
+// angularCli.install();
+console.log("Ejecting...");
 angularCli.eject();
-
-angularCli.install();
-
+console.log("Add webpack to electron...");
 setup.addElectronToWebpack();
-
-angularCli.build();
-
+// console.log("Building...");
+// angularCli.build();
+console.log("Adding election main file...");
 setup.addMainFileToDist();
-
-setup.addElectronBaseHref();
-
-// Perhaps delete old dist
-
-setup.exportDist();
+// console.log("Changing index base href...");
+// setup.addElectronBaseHref();
+// console.log("Clearing out old dist folder...");
+// // Perhaps delete old dist
+// console.log("Copying in new dist folder...");
+// setup.exportDist();
