2591906ed39105cfdb01c619aefac2fad737be04
Dolan <dolan_miu@hotmail.com>
2017-10-18 03:29:32 +0100

    Add all the other options

--- src/cli.ts
+++ src/cli.ts
@@ -57,7 +57,41 @@ const serveCommand = Yargs.command({
     describe: "Specifies app name to use.",
 }).option("environment", {
     alias: "e",
-    describe: "",
+    describe: "Defines the build environment.",
+}).option("extract-css", {
+    alias: "ec",
+    describe: "Extract css from global styles onto css files instead of js ones.",
+}).option("force", {
+    describe: "Overwrite any webpack.config.js and npm scripts already existing.",
+}).option("i18n-file", {
+    describe: "Localization file to use for i18n.",
+}).option("i18n-format", {
+    describe: "Format of the localization file specified with --i18n-file.",
+}).option("locale", {
+    describe: "Locale to use for i18n.",
+}).option("missing-translation", {
+    describe: "How to handle missing translations for i18n.",
+}).option("output-hashing", {
+    alias: "oh",
+    describe: "Define the output filename cache-busting hashing mode. Possible values: none, all, media, bundles.",
+}).option("progress", {
+    alias: "pr",
+    describe: "Log progress to the console while building.",
+}).option("sourcemap", {
+    alias: "sm",
+    describe: "Log progress to the console while building.",
+}).option("target", {
+    alias: "t",
+    describe: "Defines the build target.",
+}).option("vendor-chunk", {
+    alias: "vc",
+    describe: "Use a separate bundle containing only vendor libraries.",
+}).option("common-chunk", {
+    alias: "cc",
+    describe: "Use a separate bundle containing code used across multiple bundles.",
+}).option("verbose", {
+    alias: "v",
+    describe: "Adds more details to output logging.",
 }).argv;
 
 function processArgs(argv: {}): string[] {
